71c7c49c736a71d471576d9efeecbc68c0100e8b
will <wwrobertson1225@gmail.com>
2025-02-03 15:43:04 -0700

    updates

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "532791f9e1c9d0e93fa939aded247541"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "3ea9f3c3945c4fffb844e6ae8197ab34"
