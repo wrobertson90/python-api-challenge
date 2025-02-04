ba4001d1190f64585379b1ef68d1c7ce038c0925
wrobertson90 <wwrobertson1225@gmail.com>
2025-02-03 17:01:38 -0700

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "532791f9e1c9d0e93fa939aded247541"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "3ea9f3c3945c4fffb844e6ae8197ab34"
+geoapify_key = ""
